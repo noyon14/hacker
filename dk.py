d47553e2cafe560940e3bba16f9a83d73038a9f1
Rakibul Islam Noyon <70952823+noyon14@users.noreply.github.com>
2020-09-25 07:46:55 +0600

    Update dk.py

--- dk.py
+++ dk.py
@@ -1,7 +1,7 @@
 # hacker
-<p align="center">
+<p align="dk">
 <a href="https://github.com/hacker/followers"><img title="Followers" src="https://img.shields.io/github/followers/hacker?color=blue&style=flat-square"></a>
-<a href="https://github.com/hacker/sfteam/stargazers/"><img title="Stars" src="https://img.shields.io/github/stars/hacker/dk?color=red&style=flat-sfteam"></a>
+<a href="https://github.com/hacker/dk/stargazers/"><img title="Stars" src="https://img.shields.io/github/stars/hacker/dk?color=red&style=flat-sfteam"></a>
 <a href="https://github.com/hacker/dk/network/members"><img title="Forks" src="https://img.shields.io/github/forks/hacker/dk?color=red&style=flat-square"></a>
 <a href="https://github.com/hacker/dk/watchers"><img title="Watching" src="https://img.shields.io/github/watchers/lovefucker420/sfteam?label=Watchers&color=blue&style=flat-square"></a>
 </p>
