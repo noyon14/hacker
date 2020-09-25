6469907fa3eecc17a6d7f6b8f8ace45cf255d043
Rakibul Islam Noyon <70952823+noyon14@users.noreply.github.com>
2020-09-25 07:38:34 +0600

    Update and rename README.md to dk.py

---
+++ dk.py
@@ -0,0 +1,24 @@
+# hacker
+<p align="center">
+<a href="https://github.com/hacker/followers"><img title="Followers" src="https://img.shields.io/github/followers/hacker?color=blue&style=flat-square"></a>
+<a href="https://github.com/hacker/sfteam/stargazers/"><img title="Stars" src="https://img.shields.io/github/stars/hacker/dk?color=red&style=flat-sfteam"></a>
+<a href="https://github.com/hacker/dk/network/members"><img title="Forks" src="https://img.shields.io/github/forks/hacker/dk?color=red&style=flat-square"></a>
+<a href="https://github.com/hacker/dk/watchers"><img title="Watching" src="https://img.shields.io/github/watchers/lovefucker420/sfteam?label=Watchers&color=blue&style=flat-square"></a>
+</p>
+
+# Installation :
+
+apt update
+
+apt upgrade
+
+pkg install git
+
+pkg install python
+
+pkg install python2
+
+git clone https://github.com/noyon14/hacker.git
+
+cd hacker
+python2 dk.py
