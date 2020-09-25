37e45d3862965b2c65f704c9eeb6a73502282993
Rakibul Islam Noyon <70952823+noyon14@users.noreply.github.com>
2020-09-25 08:55:31 +0600

    Update dk.py

--- dk.py
+++ dk.py
@@ -1,24 +1,50 @@
-# hacker
-<p align="dk">
-<a href="https://github.com/hacker/followers"><img title="Followers" src="https://img.shields.io/github/followers/hacker?color=blue&style=flat-square"></a>
-<a href="https://github.com/hacker/dk/stargazers/"><img title="Stars" src="https://img.shields.io/github/stars/hacker/dk?color=red&style=flat-sfteam"></a>
-<a href="https://github.com/hacker/dk/network/members"><img title="Forks" src="https://img.shields.io/github/forks/hacker/dk?color=red&style=flat-square"></a>
-<a href="https://github.com/hacker/dk/watchers"><img title="Watching" src="https://img.shields.io/github/watchers/lovefucker420/sfteam?label=Watchers&color=blue&style=flat-square"></a>
+# hack
+
+![](https://img.shields.io/badge/noyon14-SilentForce-orange?style=for-the-badge&logo=python.svg) 
+<p align="center">
+<a href="#"><img title="Made in Bangladesh " src="https://img.shields.io/badge/MADE%20IN-Bangladesh-green?colorA=%23ff0000&colorB=%23017e40&style=for-the-badge"></a>
+</p>
+<p align="center">
+<img alt="noyon14' Github Stats" src="https://github-readme-stats.vercel.app/api?username=noyon14&show_icons=true&include_all_commits=true&hide_border=true" />
+<!--  <img alt="profile pic" width="195px" src="https://avatars2.githubusercontent.com/u/26059688?s=460&u=d41b000a62eab50d000c3da604d151cec27bd850&v=4" />  -->
+<!--  <img src="https://github-readme-stats.anuraghazra1.vercel.app/api/top-langs/?username=noyon14&hide=ruby,perl&hide_border=true" />  -->
+</p>
+<p align="center">
+<a href="#"><img title="WhatsApp Number" src="https://img.shields.io/badge/WhatsApp%20Number-+8801305055097-green?colorA=%23ff0000&colorB=%23017e40&style=for-the-badge"></a>
+</p>
+<p align="center">
+<p align="center">
+<p align="center">
+<a href="https://github.com/Noyon"><img title="noyon14" src="https://img.shields.io/badge/noyon14-Noyon-red.svg?style=for-the-badge&logo=github"></a>
+</p>
+<p align="center">
+<a href="https://github.com/noyon14/hacker/followers"><img title="Followers" src="https://img.shields.io/github/followers/noyon14?color=blue&style=flat-square"></a>
+<a href="https://github.com/noyon14/hacker/stargazers/"><img title="Stars" src="https://img.shields.io/github/stars/noyon14/hacker?color=red&style=flat-square"></a>
+<a href="https://github.com/noyon14/hacker/network/members"><img title="Forks" src="https://img.shields.io/github/forks/noyon14/hacker?color=red&style=flat-square"></a>
+<a href="https://github.com/Noyon/sf/watchers"><img title="Watching" src="https://img.shields.io/github/watchers/noyon14/hacker?label=Watchers&color=blue&style=flat-square"></a>
 </p>
 
 # Installation :
+$ `apt update`
 
-apt update
+$ `apt upgrade -y`
 
-apt upgrade
+$ `apt install git -y`
 
-pkg install git
+$ `pkg install python`
 
-pkg install python
+$ `pkg install python2 -y`
 
-pkg install python2
+$ `pip2 install requests`
 
-git clone https://github.com/noyon14/hacker.git
+$ `pip2 install mechanize`
 
-cd hacker
-python2 dk.py
+$ `pip install lolcat`
+
+$ `git clone https://github.com/noyon14/hacker.git`
+
+$ `cd hacker`
+
+$ `python2 dk.py`
+
+
